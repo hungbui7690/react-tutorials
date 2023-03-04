b8caf3d78b2ce29e3e3386d1d3a993d705797d46
Hung Bui <hungbui7690@gmail.com>
2023-03-04 15:58:36 -0700

    Reviews: How to Use "react-icons"

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 /*
-  Reviews
-  - pic: reviews-demo
+  React Icons 
+  - https://react-icons.github.io/react-icons > Search Icon
+  - Example: if icon name is FaGithubSquare > FA === Font Awesome > look for Font Awesome in the page and import
+
+  
+  (***) we can style the icons like similar text > add colors, increase size...
+  
 */
 
 import React from 'react'
 import Review from './Review'
 
+// (1)
+import { FaGithubSquare } from 'react-icons/fa'
+
 function App() {
-  return <h2>reviews project setup</h2>
+  return (
+    <>
+      <h2>reviews project setup</h2>
+
+      {/* (2) */}
+      <FaGithubSquare />
+    </>
+  )
 }
 
 export default App
