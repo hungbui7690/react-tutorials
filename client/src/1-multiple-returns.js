a0ab12054cf9b9120489b648af64ce9f27f6899c
hungbui7690 <hungbui7690@gmail.com>
2023-03-04 07:59:58 -0700

    Multiple Returns: Fetching Example P1

--- client/src/1-multiple-returns.js
+++ client/src/1-multiple-returns.js
@@ -1,16 +1,40 @@
+/*
+  - naming convention: use isLoading, isError 
+    > no error, no loading > load users
+*/
+
 import React, { useState, useEffect } from 'react'
 
 const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturns = () => {
   // (1)
-  const [loading, setLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState('default user')
 
   // (2)
-  if (loading) return <h2>Loading...</h2>
+  if (isLoading)
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    )
 
-  // (3) this is "multiple returns"
-  return <h2>multiple returns</h2>
+  // (3)
+  if (isError)
+    return (
+      <div>
+        <h2>Error...</h2>
+      </div>
+    )
+
+  // (4)
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  )
 }
 
 export default MultipleReturns
