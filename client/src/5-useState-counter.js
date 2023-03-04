6818ed7399e1d568d68243844d0c69fc8f62e5f8
hungbui7690 <hungbui7690@gmail.com>
2023-03-03 23:57:15 -0700

    useState: Counter App P2

--- client/src/5-useState-counter.js
+++ client/src/5-useState-counter.js
@@ -1,24 +1,28 @@
 import React, { useState } from 'react'
 
 const UseStateCounter = () => {
-  // (1)
   const [value, setValue] = useState(0)
 
   // (2)
+  const reset = () => {
+    setValue(0)
+  }
+
   return (
     <>
       <section style={{ margin: '0 4rem' }}>
         <h2>regular counter</h2>
-
-        {/* (a) */}
         <h1>{value}</h1>
-
-        {/* (b) */}
         <button className='btn' onClick={() => setValue(value - 1)}>
           Decrease
         </button>
 
-        {/* (c) */}
+        {/* (1) */}
+        <button className='btn' onClick={reset}>
+          {' '}
+          Reset
+        </button>
+
         <button className='btn' onClick={() => setValue(value + 1)}>
           Increase
         </button>
