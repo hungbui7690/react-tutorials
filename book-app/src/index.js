fa08026e5564c6a4bd064597b79340c23ba06014
hungbui7690 <hungbui7690@gmail.com>
2023-03-03 21:36:03 -0700

    Props Destructuring P1

--- book-app/src/index.js
+++ book-app/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-// (1) setup 2 objects
 const firstBook = {
   author: 'Amelia Hepworth',
   title: 'I Love You to the Moon and Back',
   img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL900_SR900,600_.jpg',
 }
+
 const secondBook = {
   author: 'Bill Martin Jr.',
   title: 'Brown Bear, Brown Bear, What Do You See?',
@@ -20,7 +19,7 @@ const BookList = () => {
   return (
     <section className='booklist'>
       <Book
-        img={firstBook.img} // (2) using var when we pass props
+        img={firstBook.img}
         title={firstBook.title}
         author={firstBook.author}
       />
@@ -34,12 +33,13 @@ const BookList = () => {
 }
 
 const Book = (props) => {
-  // (3) use here
+  // (***) Method 1: the first way to destructuring
+  const { img, title, author } = props
   return (
     <article className='book'>
-      <img src={props.img} alt='' width='400px' />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt='' />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   )
 }
