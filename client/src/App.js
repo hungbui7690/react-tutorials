85d6d69ef230ac663292c50faea86c359e07b6ef
Hung Bui <hungbui7690@gmail.com>
2023-03-04 15:55:06 -0700

    Reviews: Initial Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,13 @@
 /*
-  Display No Tour
-
+  Reviews
+  - pic: reviews-demo
 */
 
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-
-const url = 'https://course-api.com/react-tours-project'
+import React from 'react'
+import Review from './Review'
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [tours, setTours] = useState([])
-
-  const removeTour = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id)
-    setTours(newTours)
-  }
-
-  const fetchTours = async () => {
-    setLoading(true)
-
-    try {
-      const response = await fetch(url)
-      const tours = await response.json()
-      setLoading(false)
-      setTours(tours)
-    } catch (error) {
-      setLoading(false)
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    fetchTours()
-  }, [])
-
-  if (loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    )
-  }
-
-  // (***)
-  if (tours.length === 0)
-    return (
-      <main>
-        <div className='title'>
-          <h2>No Tours Left</h2>
-          <button className='btn' onClick={() => fetchTours()}>
-            Refresh
-          </button>
-        </div>
-      </main>
-    )
-
-  return (
-    <main>
-      <Tours tours={tours} removeTour={removeTour} />
-    </main>
-  )
+  return <h2>reviews project setup</h2>
 }
 
 export default App
