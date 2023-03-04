9e81e702af42554c14a1b370be56a262a07a0188
hungbui7690 <hungbui7690@gmail.com>
2023-03-03 21:32:12 -0700

    Props P1

--- book-app/src/index.js
+++ book-app/src/index.js
@@ -1,34 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// CSS
+// import css file > no "from" keyword
 import './index.css'
 
+// (1) setup variables
 const root = ReactDOM.createRoot(document.getElementById('root'))
+const author = 'Amelia Hepworth'
+const title = 'I Love You to the Moon and Back'
+const img =
+  'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL900_SR900,600_.jpg'
 
 const BookList = () => {
   return (
     <section className='booklist'>
       <Book />
+
+      {/* (3) pass props here */}
+      <Book job='developer' />
+      <Book title='random title' number={22} />
     </section>
   )
 }
 
-// in the {}, it must be expression > it can NOT be statement (at <p>): must return values > if we use let x = 1 > it's not ok
-const author = 'Amelia Hepworth'
-
-const Book = () => {
-  const title = 'I Love You to the Moon and Back'
+// (4) use props here
+const Book = (props) => {
+  console.log(props)
   return (
     <article className='book'>
-      <img
-        src='https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL900_SR900,600_.jpg'
-        alt=''
-        width='400px'
-      />
+      <img src={img} alt='' width='400px' />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      <p>{6 + 6}</p>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
     </article>
   )
 }
