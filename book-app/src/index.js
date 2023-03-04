d1f84d02e356659be6939a16fd2717f142ea96bd
hungbui7690 <hungbui7690@gmail.com>
2023-03-03 21:28:35 -0700

    JSX vs JS

--- book-app/src/index.js
+++ book-app/src/index.js
@@ -8,46 +8,28 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 const BookList = () => {
   return (
-    <section className='book-list'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className='booklist'>
       <Book />
     </section>
   )
 }
 
+// in the {}, it must be expression > it can NOT be statement (at <p>): must return values > if we use let x = 1 > it's not ok
+const author = 'Amelia Hepworth'
+
 const Book = () => {
+  const title = 'I Love You to the Moon and Back'
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src='https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL900_SR900,600_.jpg'
+        alt=''
+        width='400px'
+      />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      <p>{6 + 6}</p>
     </article>
-  )
-}
-
-const Image = () => {
-  return (
-    <img
-      src='https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL900_SR900,600_.jpg'
-      alt=''
-      width='400px'
-    />
-  )
-}
-
-const Title = () => <h2>I Love You to the Moon and Back</h2>
-
-const Author = () => {
-  // 1st {}: back to JS , 2nd {{}}: object
-  // > be careful when writing this way > because later, when we use library, and some libraries may use inline style > it's hard to overwrite
-  return (
-    <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
-      Amelia Hepworth
-    </h4>
   )
 }
 
