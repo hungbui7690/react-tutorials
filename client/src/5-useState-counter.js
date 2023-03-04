853e94549a59ba89b6721e77a02e9739114c392f
hungbui7690 <hungbui7690@gmail.com>
2023-03-04 00:05:06 -0700

    useState: Functional Approach P2

--- client/src/5-useState-counter.js
+++ client/src/5-useState-counter.js
@@ -1,24 +1,15 @@
-/*
-  We want to setup a function: 
-  - When we click, it will update after 2s > we want some delay
-  - With the function we wrote below:
-    + click once > OK 
-    + click twice or more > after 2s, just update +1
-  
-  >> Reason: because setValue() is sync > so when we click 3 times, react still thinks the previous value = 0
-
-  (***) in this lesson, we learn how to pass function into setValue()
-*/
-
 import React, { useState } from 'react'
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0)
 
-  // (1) to solve this problem below, we need to use Functional Approach > next lecture
+  // (***) in setValue(), we pass function instead of passing "value+1" like previous lecture
+  // name convention = prevState or prev or oldState
   const complexIncrease = () => {
     setTimeout(() => {
-      setValue(value + 1)
+      setValue((prevState) => {
+        return prevState + 1
+      })
     }, 2000)
   }
 
@@ -27,8 +18,6 @@ const UseStateCounter = () => {
       <section style={{ margin: '0 4rem' }}>
         <h2>more complex couter</h2>
         <h1>{value}</h1>
-
-        {/* (2) */}
         <button className='btn' onClick={complexIncrease}>
           Increase Later
         </button>
