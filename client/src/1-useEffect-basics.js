3e4d5c8d980b69f97352abce23e265cb7340d3c4
hungbui7690 <hungbui7690@gmail.com>
2023-03-04 01:21:47 -0700

    useEffect: Basics P2

--- client/src/1-useEffect-basics.js
+++ client/src/1-useEffect-basics.js
@@ -1,49 +1,38 @@
 /*
-  In React, useState() and useEffect() are the most used hooks
-
-  useEffect(): 
-  - allow us to do the side effects (any work outside of the component > maybe change document title, signing up for subscription, fetching data, setting up an event listener ...) 
-
-  - by default, useEffect runs after every re-render 
-
-  - Usage: 
-    + cleanup function
-    + second parameter
-
-/////////////////////////////////////////////////////
-
-  Log out 'render component' twice because in index.js, we have StrictMode tag: 
-    ReactDOM.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-      document.getElementById('root')
-    )
-
-  If we want to show once > remove StrictMode tag: 
-    ReactDOM.render(
-        <App />,
-      document.getElementById('root')
-    )
+  When we click the button, we call setValue()
   
+  > setValue() does 2 things: 
+    + (1) it preserved the value between the renders
+    + (2) it triggered the render 
 
-  > In this case, we can see that console.log() is the side effect 
+  > Because (2) triggers re-render > useEffect() will be called automatically
 
 */
+import React, { useState, useEffect } from 'react'
 
-// (2) import {useEffect}
-import React, { useEffect } from 'react'
-
-// (3)
 const UseEffectBasics = () => {
+  const [value, setValue] = useState(0)
+
+  // (2) side effect: change document title
   useEffect(() => {
-    console.log('call useEffect') // runs every render
+    console.log('call useEffect')
+
+    // (***)
+    document.title = `New Messages(${value})`
   })
 
-  // (1)
-  console.log('render component') // run first
+  console.log('render component')
 
-  return <h2>useEffect Basics</h2>
+  return (
+    <React.Fragment>
+      <h1>{value}</h1>
+
+      {/* (1) */}
+      <button className='btn' onClick={() => setValue(value + 1)}>
+        Increase
+      </button>
+    </React.Fragment>
+  )
 }
 
 export default UseEffectBasics
