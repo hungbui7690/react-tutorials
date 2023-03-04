23a2f0770cc416cd82e11cd780e5124c994e0fd4
hungbui7690 <hungbui7690@gmail.com>
2023-03-03 23:45:27 -0700

    useState with array P3: add clear button

--- client/src/3-useState-array.js
+++ client/src/3-useState-array.js
@@ -1,9 +1,5 @@
 /*
-  - React.useState() > when we write like this, we don't need to import
-    > if we use useState() many times, then import > otherwise, use this way
 
-  - we using onClick with parameters > need to use arrow function 
-    > if we don't use arrow function > function will be invoked right after component is rendered
 */
 
 import React from 'react'
@@ -12,7 +8,6 @@ import { data } from './data' // (***)
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data)
 
-  // (1)
   const removeItem = (id) => {
     let newPeople = people.filter((person) => person.id !== id)
     setPeople(newPeople)
@@ -25,12 +20,15 @@ const UseStateArray = () => {
         return (
           <div key={id} className='item'>
             <h4>{name}</h4>
-
-            {/* (2) */}
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         )
       })}
+
+      {/* (***) */}
+      <button className='btn' onClick={() => setPeople([])}>
+        Clear Items
+      </button>
     </React.Fragment>
   )
 }
