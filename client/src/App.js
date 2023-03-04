54933cc5195aea66362e5b3c7cf56b66d372298e
Hung Bui <hungbui7690@gmail.com>
2023-03-04 16:24:11 -0700

    Accordion: Basic Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,31 @@
 /*
-  Accordion: Initial Setup
-  - Pic: accordion-demo
-  
+  Accordion: Basic Setup
+  - in this project, instead having Question[s] & Question > we just have Question.js 
+    > because of that, we will using map() to iterate the questions in App.js
 
 */
 
 import React, { useState } from 'react'
-import data from './data'
 import SingleQuestion from './Question'
 
+// (1)
+import data from './data'
+
 function App() {
-  return <h2>accordion project setup</h2>
+  // (2)
+  const [questions, setQuestions] = useState(data)
+
+  return (
+    <div className='container'>
+      <h3>Questions and Answers about Login</h3>
+      <div className='section info'>
+        {/* (3) pass sang bên Question.js (Single Question) */}
+        {questions.map((question) => {
+          return <SingleQuestion key={question.id} {...question} />
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default App
