8baa9a3da7fe0297a7817fb9189a12ea50488d2a
Hung Bui <hungbui7690@gmail.com>
2023-03-04 14:05:34 -0700

    Tours: Fetching Data P3: Catching Error

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 /*
-  - Fetching Tours
+  Catching Error
+  - if there is some error, we still setLoading = false, but log the error out or setError = true
+
+  > after finished, go to RDT (React Dev Tool) to test the state
+    > state:
+      - from [] to ([{…},  {…}, {…}, {…}, {…}])
+      - from true to false
+      > pic: test
 */
 
 import React, { useState, useEffect } from 'react'
@@ -12,20 +19,30 @@ function App() {
   const [loading, setLoading] = useState(true)
   const [tours, setTours] = useState([])
 
-  // (1) create function to fetch data
   const fetchTours = async () => {
-    // (***)
+    // (a) loading...
     setLoading(true)
 
-    const response = await fetch(url)
-    const tours = await response.json()
-    console.log(tours)
+    try {
+      // (a)
+      const response = await fetch(url)
+      const tours = await response.json()
+
+      // (b) if the code run to this line, which means there is no error > so that we setLoading = false
+      setLoading(false)
+
+      // (c)
+      setTours(tours)
+    } catch (error) {
+      // (***) catching error > normally, we will setError = true here if we have the error state
+      setLoading(false)
+      console.log(error)
+    }
   }
 
-  // (2) use useEffect() to call function to fetch data
   useEffect(() => {
     fetchTours()
-  }, []) // (***)
+  }, [])
 
   if (loading) {
     return (
