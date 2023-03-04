3f067b8a52973dc09115ad477f609b14e916f386
hungbui7690 <hungbui7690@gmail.com>
2023-03-04 01:24:55 -0700

    useEffect: Conditional

--- client/src/1-useEffect-basics.js
+++ client/src/1-useEffect-basics.js
@@ -1,24 +1,30 @@
 /*
-  When we click the button, we call setValue()
-  
-  > setValue() does 2 things: 
-    + (1) it preserved the value between the renders
-    + (2) it triggered the render 
+  - now, we want to trigger useEffect() when value > 0
 
-  > Because (2) triggers re-render > useEffect() will be called automatically
+  - error if we write this way 
+      if (value > 0) {
+        useEffect(() => {
+          console.log('call useEffect')
+          document.title = `New Messages(${value})`
+        })
+      }
+    > we learned that we can not use conditional function with hooks > this is an example
+
+  > to fix, we will setup if/else inside useEffect() (below)
 
 */
 import React, { useState, useEffect } from 'react'
 
 const UseEffectBasics = () => {
+  // (1)
   const [value, setValue] = useState(0)
 
-  // (2) side effect: change document title
+  // (3)
   useEffect(() => {
     console.log('call useEffect')
 
-    // (***)
-    document.title = `New Messages(${value})`
+    // (***) we must write if / else inside of useEffect()
+    if (value > 1) document.title = `New Messages(${value})`
   })
 
   console.log('render component')
@@ -27,7 +33,7 @@ const UseEffectBasics = () => {
     <React.Fragment>
       <h1>{value}</h1>
 
-      {/* (1) */}
+      {/* (2) */}
       <button className='btn' onClick={() => setValue(value + 1)}>
         Increase
       </button>
