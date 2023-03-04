29374190b9a812dedd4400597bcae146d9c81c23
hungbui7690 <hungbui7690@gmail.com>
2023-03-04 08:47:13 -0700

    Show/Hide Components P2

--- client/src/3-show-hide.js
+++ client/src/3-show-hide.js
@@ -1,34 +1,53 @@
 /*
-  Now, we can use "Short Circuit" to show/hide item 
-  > but now, we are missing Clean-Up
+  - we have Memory Leak
+
+  > without [] in dep list
+    - open app 
+    - choose Element tab, Styles sub-tab
+    - click Show/Hide
+    - Resize 
+    - choose Event Listeners sub-tab
+
+  > with [] in dep list: 
+    - do the same but click show/hide, then resize
+    - after that, open Event Listeners sub-tab
+    > we can see that though we have [], it it still create event every time we click on show/hide and resize
 */
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const ShowHide = () => {
-  // (2)
   const [show, setShow] = useState(false)
 
-  // (3)
   return (
-    <React.Fragment>
-      {/* (b) */}
+    <>
       <button className='btn' onClick={() => setShow(!show)}>
         Show/Hide
       </button>
-
-      {/* (a) */}
       {show && <Item />}
-    </React.Fragment>
+    </>
   )
 }
 
-// (1)
 const Item = () => {
+  // (1)
+  const [size, setSize] = useState(window.innerWidth)
+
+  // (2)
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+
+  // (3) checkSize runs continuously when size is changing
+  useEffect(() => {
+    console.log('abc')
+    window.addEventListener('resize', checkSize)
+  }, []) // (***) there's no dep list
+
   return (
     <div style={{ marginTop: '2rem' }}>
       <h1>window</h1>
-      <h2>size: </h2>
+      <h2>size: {size}</h2>
     </div>
   )
 }
