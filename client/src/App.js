d3449a9950df1c4683d3c4bcb26f37def79cd9dc
hungbui7690 <hungbui7690@gmail.com>
2023-03-04 09:06:31 -0700

    Tours: Fetching Data P1: Setup Loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,36 @@
 /*
   Tours Project
-  - requirement > check pic: tours-demo + tours-api-structure
-
+  - Setup Loading
 */
 
 import React, { useState, useEffect } from 'react'
+
+// (1)
 import Loading from './Loading'
 import Tours from './Tours'
 
-// (***)
 const url = 'https://course-api.com/react-tours-project'
 
 function App() {
-  return <h2>Tours Project Setup</h2>
+  // (2a)
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([]) // (***) empty array
+
+  // (2b)
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  // (2c)
+  return (
+    <main>
+      <Tours />
+    </main>
+  )
 }
 
 export default App
