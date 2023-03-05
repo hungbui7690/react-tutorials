a5f6255a1de30ad676a8f45601186d7123d92182
Hung Bui <hungbui7690@gmail.com>
2023-03-05 14:55:55 -0700

    useRef: Navbar: Simple Toggle

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,48 +1,40 @@
-/*
-  
-//////////////////////////////////////////////////////////////////
-
-  (1) check file data.jsx > then we use map() to render the data 
-  (2) create Sidebar.jsx (this is an example file, we just use it for this lesson)
-  (3) import in App.js 
-  (4) setup in Sidebar.jsx 
-  (5) go to data.jsx and make some change > we will see both sidebar and navbar will be changed > this is the benefit when having data in 1 place 
-  (6) do the same with Social Icons 
-
-  (***) (small screen) we can see that in data.jsx, there are 5 links, but after this lecture, it just shows 4 links > set height in css file
-*/
-
 import React, { useState, useRef, useEffect } from 'react'
 import { FaBars, FaTwitter } from 'react-icons/fa'
+import { links, social } from './data'
 import logo from './logo.svg'
 
-import { links, social } from './data' // (***)
-
 const Navbar = () => {
+  // (1)
+  const [showLinks, setShowLinks] = useState(false)
+
   return (
     <div className='nav-center'>
       <div className='nav-header'>
         <img src={logo} alt='logo' />
-        <button className='nav-toggle'>
+
+        {/* (2) */}
+        <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
           <FaBars />
         </button>
       </div>
 
-      <div className='links-container show-container'>
-        <ul className='links'>
-          {/* (1a) */}
-          {links.map((link) => {
-            const { id, url, text } = link
-            return (
-              <li key={id}>
-                <a href={url}>{text}</a>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+      {/* (3) put the div in conditional rendering */}
+      {showLinks && (
+        <div className='links-container show-container'>
+          <ul className='links'>
+            {links.map((link) => {
+              const { id, url, text } = link
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      )}
+
       <ul className='social-icons'>
-        {/* (1b) create Sidebar.js */}
         {social.map((socialIcon) => {
           const { id, url, icon } = socialIcon
           return (
