4fd4aa2e85889688096d68f267032422ed182b97
Hung Bui <hungbui7690@gmail.com>
2023-03-04 20:20:58 -0700

    Sliders: Initial Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,81 +1,14 @@
 /*
-  Tabs: Display Buttons
-
+  Sliders: Initial Setup
 */
 
 import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-
-const url = 'https://course-api.com/react-tabs-project'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
+import { FaQuoteRight } from 'react-icons/fa'
+import data from './data'
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [jobs, setJobs] = useState([])
-  const [value, setValue] = useState(0) // (***) we need to use value here, since below we use "index" when render
-
-  const fetchJobs = async () => {
-    const response = await fetch(url)
-    const newJobs = await response.json()
-    console.log(newJobs)
-
-    setJobs(newJobs)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    fetchJobs()
-  }, [])
-
-  if (loading) {
-    return (
-      <section className='section loading'>
-        <h1>Loading...</h1>
-      </section>
-    )
-  }
-
-  const { company, dates, duties, title } = jobs[value]
-
-  return (
-    <section className='section'>
-      <div className='title'>
-        <h2>experience</h2>
-        <div className='underline'></div>
-      </div>
-
-      <div className='jobs-center'>
-        {/* (1) setup buttons */}
-        <div className='btn-container'>
-          {jobs.map((item, index) => {
-            return (
-              <button
-                key={index}
-                onClick={() => setValue(index)}
-                className={`job-btn ${index === value && 'active-btn'} `}
-              >
-                {item.company}
-              </button>
-            )
-          })}
-        </div>
-
-        {/* (2) job info */}
-        <article className='job-info'>
-          <h3>{title}</h3>
-          <h4>{company}</h4>
-          <p className='job-date'>{dates}</p>
-          {duties.map((duty, index) => {
-            return (
-              <div key={index} className='job-desc'>
-                <FaAngleDoubleRight className='job-icon' />
-                <p>{duty}</p>
-              </div>
-            )
-          })}
-        </article>
-      </div>
-    </section>
-  )
+  return <h2>slider project setup</h2>
 }
 
 export default App
