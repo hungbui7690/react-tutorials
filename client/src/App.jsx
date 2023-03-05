a9cde2b9409787f754e6cd6bc8e04382ecf064b1
Hung Bui <hungbui7690@gmail.com>
2023-03-05 04:00:06 -0700

    Colors Generator: Values & Error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
 /*
-  Colors Generator: Structure
-  - use value & onChange > controlled input
+  Colors Generator: Values & Error
 
 */
 
 import React, { useState } from 'react'
 import SingleColor from './SingleColor'
-
 import Values from 'values.js'
 
 function App() {
@@ -17,11 +15,16 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    let colors = new Values(color).all(10)
-    console.log(colors)
+    // (1) put the try/catch to prevent app from crashing when the color code is not correct/empty
+    try {
+      let colors = new Values(color).all(10)
+      console.log(colors) // (***) actually, we have 21 items in this array > original color + 10 tins + 10 shades
+    } catch (error) {
+      console.log(error)
+      setError(true)
+    }
   }
 
-  // (***)
   return (
     <React.Fragment>
       <section className='container'>
@@ -30,15 +33,15 @@ function App() {
           <input
             type='text'
             placeholder='#f15025'
-            value={color} // (a) hook input to state
-            onChange={(e) => setColor(e.target.value)} // (b) hook input to state
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            className={`${error ? 'error' : null}`} // (2)
           />
           <button type='submit' className='btn'>
             Submit
           </button>
         </form>
       </section>
-
       <section className='colors'>
         <h4>List goes here</h4>
       </section>
