9c169346b440dbd71be4db355484b5b8cd9dae56
Hung Bui <hungbui7690@gmail.com>
2023-03-04 20:11:26 -0700

    Tabs: Fetch Jobs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 /*
-  Tabs: Initial Setup
-  - check pic
-  - data will be fetched from the link below
+  Tabs: Fetch Jobs
 
 */
 
@@ -11,6 +9,37 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 const url = 'https://course-api.com/react-tabs-project'
 
 function App() {
+  // (1)
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [index, setIndex] = useState(0)
+
+  // (2)
+  const fetchJobs = async () => {
+    // (a)
+    const response = await fetch(url)
+    const newJobs = await response.json()
+    console.log(newJobs)
+
+    // (b) if code is at this point > no error > we should setLoading = false first
+    setLoading(false)
+    setJobs(newJobs)
+  }
+
+  // (3) use useFetch() to call the function to initial data + re-render
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+
+  // (4)
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+
   return <h2>tabs project setup</h2>
 }
 
