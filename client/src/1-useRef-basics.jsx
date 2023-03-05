e4a6f4cc64190714a4e361f998e9d227554b0e4f
Hung Bui <hungbui7690@gmail.com>
2023-03-05 06:58:57 -0700

    useRef: Initial Setup

---
+++ client/src/1-useRef-basics.jsx
@@ -0,0 +1,16 @@
+/*
+  useRef: Initial Setup
+  > Characteristics:  
+    - preserves value
+    - DOES NOT trigger re-render
+    - target DOM nodes/elements
+
+*/
+
+import React, { useEffect, useRef } from 'react'
+
+const UseRefBasics = () => {
+  return <h2>useRef</h2>
+}
+
+export default UseRefBasics
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,113 +1,11 @@
-/*
-  Grocery Bud: Local Storage
-  - now we want to save the list in localStorage because we don't want to lose the item after refreshing our app
-
-  > we want to update the localStorage every time there are some changes in the list > use useEffect(, [list]) to achieve that 
- */
-
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
-
-// (2) load from local storage
-const getLocalStorage = () => {
-  let list = localStorage.getItem('list')
-  if (list) return JSON.parse(list)
-  else return []
-}
+import React from 'react'
+import Setup from './1-useRef-basics'
 
 function App() {
-  const [name, setName] = useState('')
-  const [list, setList] = useState(getLocalStorage()) // (3) use here
-  const [isEditing, setIsEditing] = useState(false)
-  const [editID, setEditID] = useState(null)
-
-  const [alert, setAlert] = useState({
-    show: false,
-    msg: '',
-    type: '',
-  })
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    if (!name) {
-      showAlert(true, 'danger', 'please enter value')
-    } else if (name && isEditing) {
-      setList(
-        list.map((item) => {
-          if (item.id === editID) {
-            return { ...item, title: name }
-          }
-          return item
-        })
-      )
-      setName('')
-      setEditID(null)
-      setIsEditing(false)
-      showAlert(true, 'success', 'value changed')
-    } else {
-      showAlert(true, 'success', 'item added to the list')
-      const newItem = { id: new Date().getTime().toString(), title: name }
-      setList([...list, newItem])
-      setName('')
-    }
-  }
-
-  const showAlert = (show = false, type = '', msg = '') => {
-    setAlert({ show, type, msg })
-  }
-
-  const clearList = () => {
-    setList([])
-    showAlert(true, 'danger', 'empty list')
-  }
-
-  const removeItem = (id) => {
-    showAlert(true, 'danger', 'item removed')
-    setList(list.filter((item) => item.id !== id))
-  }
-
-  const editItem = (id) => {
-    const specificItem = list.find((item) => item.id === id)
-    setIsEditing(true)
-    setEditID(id)
-    setName(specificItem.title)
-  }
-
-  // (1) local storage
-  useEffect(() => {
-    localStorage.setItem('list', JSON.stringify(list)) // (***) we can only store string in local storage
-  }, [list]) // (***)
-
   return (
-    <section className='section-center'>
-      <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
-        <h3>grocery bud</h3>
-        <div className='form-control'>
-          <input
-            type='text'
-            className='grocery'
-            placeholder='e.g. eggs'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <button type='submit' className='submit-btn'>
-            {isEditing ? 'edit' : 'submit'}
-          </button>
-        </div>
-      </form>
-
-      {list.length > 0 && (
-        <div className='grocery-container'>
-          <List items={list} removeItem={removeItem} editItem={editItem} />
-          <button className='clear-btn' onClick={clearList}>
-            Clear Items
-          </button>
-        </div>
-      )}
-    </section>
+    <div className='container'>
+      <Setup />
+    </div>
   )
 }
 
