8ed7afb6593cb5a79cb5da25832f6ddb719a98d4
Hung Bui <hungbui7690@gmail.com>
2023-03-04 20:08:20 -0700

    Tabs: Initial Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,17 @@
 /*
-  Menu: Buttons (Dynamic Approach)
+  Tabs: Initial Setup
+  - check pic
+  - data will be fetched from the link below
+
 */
 
-import React, { useState } from 'react'
-import Menu from './Menu'
-import Categories from './Categories'
-import items from './data'
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
 
-// (1) get all unique categories >> new Set returns object: {'breakfast', 'lunch', 'shakes'} > we have to use spread operator to convert it into array
-const allCategories = ['all', ...new Set(items.map((item) => item.category))]
-console.log(allCategories)
+const url = 'https://course-api.com/react-tabs-project'
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState(allCategories) // (2) use here
-
-  // (3)
-  const filterItems = (category) => {
-    if (category === 'all') {
-      setMenuItems(items)
-      return
-    }
-
-    // (***) this must be items.filter(), not menuItems.filter() like the prev lessons (we don't use state, but use the original data)
-    const newItems = items.filter((item) => item.category === category)
-    setMenuItems(newItems)
-  }
-
-  return (
-    <main>
-      <section className='menu section'>
-        <div className='title'>
-          <h2>our menu</h2>
-          <div className='underline'></div>
-        </div>
-
-        {/* (4) */}
-        <Categories filterItems={filterItems} categories={categories} />
-        <Menu items={menuItems} />
-      </section>
-    </main>
-  )
+  return <h2>tabs project setup</h2>
 }
 
 export default App
