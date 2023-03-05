165df8110aa90cfe9f9fd109d7579f78c7d6738b
Hung Bui <hungbui7690@gmail.com>
2023-03-05 03:44:05 -0700

    Lorem: Complete P1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,20 @@
 /*
-  Lorem: Structure
-  - in our project, if we enter negative value > generate only 1 paragraph, and maximum is 8 paragraphs > because in data.js file, we just have only an array of 8 paragraphs
-
-  - States:
-    + count: how many paragraphs that we want 
-    + text: or paragraphs > array of paragraphs 
-
-  (***) Again: when we add "value" and "onchange" on inputs > they become "controlled inputs"
-
+  Lorem: Complete P1
 */
 
 import React, { useState } from 'react'
 import data from './data'
 
 function App() {
-  // (1) setup states
   const [count, setCount] = useState(0)
   const [text, setText] = useState([])
 
-  // (2)
+  // (1)
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hello world')
+    setText(data) // use all data
   }
 
-  // (3)
   return (
     <section className='section-center'>
       <h3>tired of boring lorem ipsum? </h3>
@@ -42,19 +32,11 @@ function App() {
         </button>
       </form>
 
-      {/* dummy data for initial render only */}
+      {/* (2) */}
       <article className='lorem-text'>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure
-          aspernatur officiis quidem qui voluptatem? Assumenda nam non tempore
-          ipsum ab ratione, ipsam soluta. Enim dolorem, praesentium recusandae
-          perferendis provident explicabo!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique sit
-          dolores laborum nisi natus, ullam commodi saepe eveniet quis atque
-          alias animi, voluptates rem expedita hic tenetur officiis amet. Est!
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
       </article>
     </section>
   )
