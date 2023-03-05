cc3540ad46929f9e136d33c2a41b386f05d8421d
Hung Bui <hungbui7690@gmail.com>
2023-03-05 16:05:31 -0700

    useReducer: useState Setup P1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,9 @@
 /*
-  useReducer: Initial Setup 
+  useReducer: useState Setup P1
+  
+  (1) work in setup/index.jsx
 
-  - useReducer() hook is used when we have complicated setup
-    > with some simple app like to list, use useState() is enough, we don't have to use useReducer()
 
-  - but when our app gets more complicated, it's a good practice to use useReducer > because it add more structure to the state, so that we cannot add or change the states easily > we must follow the structure to do that 
-    > we must go through certain steps 
-    > it's useful when we work with team.
-
-//////////////////////////////////////////////////////////
-
-  - folder structure > we have /setup folder > contains: index.jsx & Modal.jsx
-    > because we have index.jsx in /setup > when we import into App.js, we don't have to specify the exact file, but just need to import folder 
-      + /setup/index.jsx imports Modals.js 
-      + App.js just need to import /setup > and then we can use Modal.jsx
-    > because if any folder that has index.jsx > that index.jsx file will become the main entry point of that folder
 
 */
 
--- client/src/setup/index.jsx
+++ client/src/setup/index.jsx
@@ -1,10 +1,44 @@
+/*
+  
+  - check requirement.png and guess how many states that we need > and which way we need to use to get the input
+
+  (1) create states in index.jsx
+  (2) setup return (form và input > use controlled input)
+  (3) handleSubmit() 
+
+*/
+
 import React, { useState, useReducer } from 'react'
 import Modal from './Modal'
 import { data } from '../data'
-// reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>
+  // (1)
+  const [name, setName] = useState('')
+  const [people, setPeople] = useState(data)
+  const [showModal, setShowModel] = useState(false)
+
+  // (3)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  // (2)
+  return (
+    <React.Fragment>
+      {showModal && <Modal />}
+      <form onSubmit={handleSubmit} className='form'>
+        <div>
+          <input
+            type='text value '
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type='submit'>add</button>
+      </form>
+    </React.Fragment>
+  )
 }
 
 export default Index
