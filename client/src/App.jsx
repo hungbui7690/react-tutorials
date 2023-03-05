7e4b97b79404ba7d895b3c4e00e4da8418abec36
Hung Bui <hungbui7690@gmail.com>
2023-03-05 14:52:57 -0700

    useRef: Navbar: Data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,27 @@
 /*
-  useRef: Navbar: Structure P2
+  useRef: Navbar: Data
   
-  (2) Navbar.jsx
+  - The problem with our app is we hard code the links below (home/about/contact/products) > when app gets bigger, it is hard to update
+    > for example, it we add sidebar, footer > and in those sections have links that are exactly like navbar > every time we make some update in url or link > we need to change in 3 places
+  
+  - with big project > we should have data in 1 place > and every time we want to update, we just need to change in 1 place > in this project, it is data.jsx
+
+  (1) Navbar.js
 */
 
 import React from 'react'
 import Navbar from './Navbar'
 
+// (3a)
+import Sidebar from './Sidebar'
+
 function App() {
   return (
     <>
       <Navbar />
+      <br /> <br /> <hr />
+      {/* (3b) */}
+      <Sidebar />
     </>
   )
 }
--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,7 +1,22 @@
+/*
+  
+//////////////////////////////////////////////////////////////////
+
+  (1) check file data.jsx > then we use map() to render the data 
+  (2) create Sidebar.jsx (this is an example file, we just use it for this lesson)
+  (3) import in App.js 
+  (4) setup in Sidebar.jsx 
+  (5) go to data.jsx and make some change > we will see both sidebar and navbar will be changed > this is the benefit when having data in 1 place 
+  (6) do the same with Social Icons 
+
+  (***) (small screen) we can see that in data.jsx, there are 5 links, but after this lecture, it just shows 4 links > set height in css file
+*/
+
 import React, { useState, useRef, useEffect } from 'react'
 import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
 import logo from './logo.svg'
+
+import { links, social } from './data' // (***)
 
 const Navbar = () => {
   return (
@@ -13,41 +28,29 @@ const Navbar = () => {
         </button>
       </div>
 
-      {/* (1) we haven't learned React Router, so we use <a> > because of that, it will show warning > since in react, <a> is only used for external links > not for internal links  */}
       <div className='links-container show-container'>
         <ul className='links'>
-          <li>
-            <a href='#'>Home</a>
-          </li>
-          <li>
-            <a href='#'>About</a>
-          </li>
-          <li>
-            <a href='#'>Contact</a>
-          </li>
-          <li>
-            <a href='#'>Products</a>
-          </li>
+          {/* (1a) */}
+          {links.map((link) => {
+            const { id, url, text } = link
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            )
+          })}
         </ul>
       </div>
-
-      {/* (2) social icons: only shown in big screen */}
       <ul className='social-icons'>
-        <li>
-          <a href='https://www.twitter.com'>
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a href='https://www.twitter.com'>
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a href='https://www.twitter.com'>
-            <FaTwitter />
-          </a>
-        </li>
+        {/* (1b) create Sidebar.js */}
+        {social.map((socialIcon) => {
+          const { id, url, icon } = socialIcon
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}{' '}
       </ul>
     </div>
   )
