e0559a77857aca2343a3ec535905e2ac10105a05
Hung Bui <hungbui7690@gmail.com>
2023-03-05 16:15:43 -0700

    useReducer: Refactor to useReducer P1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 /*
-  useReducer: useState Setup P2
-  - when we give the name to input, it will be saved into people array below, and also display to modal > (check requirement.png)
+  useReducer: Work with useReducer
 
   (1) work in setup/index.jsx
 
--- client/src/setup/index.jsx
+++ client/src/setup/index.jsx
@@ -1,34 +1,51 @@
 /*
-  - this is how we setup this app using useState() 
-    > next lecture, we will use useReducer()
+  (1) remove state people and setModal
 
+  - when we call useState() > we receive 1 state + 1 function: 
+    > const [name, setName] = useState('') 
+      > sẽ get đc name (state) và setName() 
+
+  - when we call useReducer() > we also receive a state and a function: 
+    > state & dispatch()
+
+  (2) create reducer
+  (3) create default state
+  (4) useReducer(reducer, defaultState)
+  (5) use state.isModalOpen with conditional rendering 
+  (6) add state.people
  */
 
 import React, { useState, useReducer } from 'react'
 import Modal from './Modal'
 import { data } from '../data'
 
+// (2)
+const reducer = (state, action) => {}
+
+// (3) initial state for useReducer > we will put all global states in this object > "name" is only used locally, so that we use useState()
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: 'hello world',
+}
+
 const Index = () => {
+  // (1) remove people and Modal states from useState() > we will put it in defaultState object
   const [name, setName] = useState('')
-  const [people, setPeople] = useState(data)
-  const [showModal, setShowModel] = useState(false)
+  const [state, dispatch] = useReducer(reducer, defaultState) // (4) use useReducer()
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    // (1) handle submit > then display in (2)
     if (name) {
-      setShowModel(true)
-      setPeople([...people, { id: new Date().getTime().toString(), name }])
-      setName('')
     } else {
-      setShowModel(true)
     }
   }
 
   return (
     <React.Fragment>
-      {showModal && <Modal />}
+      {/* (5) use state.isModalOpen */}
+      {state.isModalOpen && <Modal />}
       <form onSubmit={handleSubmit} className='form'>
         <div>
           <input
@@ -40,8 +57,8 @@ const Index = () => {
         <button type='submit'>add</button>
       </form>
 
-      {/* (2) display people */}
-      {people.map((person) => {
+      {/* (6) use state.people */}
+      {state.people.map((person) => {
         return (
           <div key={person.id}>
             <h4>{person.name}</h4>
