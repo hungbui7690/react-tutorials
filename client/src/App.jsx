dad3253d509ff620d0b13e7662c55f5babf81046
Hung Bui <hungbui7690@gmail.com>
2023-03-05 05:59:09 -0700

    Grocery Bud: Initial Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,61 +1,9 @@
-/*
-  Colors Generator: Copy to Clipboard P2
-
-*/
-
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
-import Values from 'values.js'
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
 
 function App() {
-  const [color, setColor] = useState('')
-  const [error, setError] = useState(false)
-  const [list, setList] = useState([])
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    try {
-      let colors = new Values('lightpink').all(10)
-      setList(colors)
-      setError(false) // (1)
-    } catch (error) {
-      console.log(error)
-      setError(true)
-    }
-  }
-
-  return (
-    <React.Fragment>
-      <section className='container'>
-        <h3>Color Generator</h3>
-        <form onSubmit={handleSubmit}>
-          <input
-            type='text'
-            placeholder='#f15025'
-            value={color}
-            onChange={(e) => setColor(e.target.value)}
-            className={`${error ? 'error' : null}`}
-          />
-          <button type='submit' className='btn'>
-            Submit
-          </button>
-        </form>
-      </section>
-      <section className='colors'>
-        {list.map((color, index) => {
-          return (
-            <SingleColor
-              key={index}
-              {...color}
-              index={index}
-              hexColor={color.hex}
-            />
-          )
-        })}
-      </section>
-    </React.Fragment>
-  )
+  return <h2>grocery bud setup</h2>
 }
 
 export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,9 @@
+/*
+  Grocery Bud: Initial Setup
+  - pic
+  
+*/
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
