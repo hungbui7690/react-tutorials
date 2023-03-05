dcaa7c278cef62e61b212a305af68349841847a1
Hung Bui <hungbui7690@gmail.com>
2023-03-05 14:36:28 -0700

    useRef: Navbar: Initial Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,16 @@
 /*
-   Accordion: Logic P2
-  - right now, if we click on the different, it still close the old one, but there are some problems: 
-    [1] we can not close the opening element when we click on it
-    [2] the (-) sign does not switch to (+) after closing
-
+  useRef: Navbar: Initial Setup
+  - pic
 */
 
-import React, { useState } from 'react'
-import SingleQuestion from './Question'
-import data from './data'
+import React from 'react'
+import Navbar from './Navbar'
 
 function App() {
-  const [questions, setQuestions] = useState(data)
-  const [prevElement, setPrevElement] = useState(null)
-
   return (
-    <div className='container'>
-      <h3>Questions and Answers about Login</h3>
-      <section className='info'>
-        {questions.map((question) => {
-          return (
-            <SingleQuestion
-              key={question.id}
-              {...question}
-              prevElement={prevElement}
-              setPrevElement={setPrevElement}
-            />
-          )
-        })}
-      </section>
-    </div>
+    <>
+      <h2>navbar project setup</h2>
+    </>
   )
 }
 
