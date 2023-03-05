36babd273877fe35485b1f7bcee292adaf0cd7eb
Hung Bui <hungbui7690@gmail.com>
2023-03-04 20:23:59 -0700

    Sliders: Basic Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
 /*
-  Sliders: Initial Setup
+  Sliders: Basic Setup
+  - check pictures
+  - in browser, we see all items are placed on top of each other, because we comment some codes in .css file
+    > we do this because we want to understand how slider works
+
 */
 
 import React, { useState, useEffect } from 'react'
@@ -8,7 +12,41 @@ import { FaQuoteRight } from 'react-icons/fa'
 import data from './data'
 
 function App() {
-  return <h2>slider project setup</h2>
+  // (1)
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(0)
+
+  // (2)
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className='section-center'>
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person
+
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className='person-img' />
+              <h4>{name}</h4>
+              <p className='title'>{title}</p>
+              <p className='text'>{quote}</p>
+              <FaQuoteRight />
+            </article>
+          )
+        })}
+        <button className='prev'>
+          <FiChevronLeft />
+        </button>
+        <button className='next'>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  )
 }
 
 export default App
