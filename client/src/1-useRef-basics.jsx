c5fcc67ae69e92cb022a4800bc6690e1490a4c24
Hung Bui <hungbui7690@gmail.com>
2023-03-05 07:08:49 -0700

    useRef: Basics P2

--- client/src/1-useRef-basics.jsx
+++ client/src/1-useRef-basics.jsx
@@ -1,44 +1,38 @@
 /*
-  useRef: Basics P1
-  - when we learn about form, we learn about controlled inputs > inputs which are not controlled by themselves 
-    > in this tutorial, we will learn another way: useRef()
-    > there are many things that we can do with useRef() > but most common is "target DOM nodes/elements"
-  
-  useRef: 
-  + preserves value (similar useState)
-  + NOT trigger re-render
-  + target DOM nodes/elements 
-
-  (***) in the below example, we don't use controlled inputs > NO "value", "onChange", "state"
-
-/////////////////////////////////////////
-
-  refContainer.current
-  refContainer.current.value
+  useRef: Basics P2
 
 */
 
 import React, { useEffect, useRef } from 'react'
 
 const UseRefBasics = () => {
-  const refContainer = useRef(null) // (1) default value = null
+  const refContainer = useRef(null)
+  const divContainer = useRef(null) // (1) we can create ref to any element > not only input, but any element
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(refContainer.current) // (3a) return that element
-    console.log(refContainer.current.value) // (3b) the element that we targets is input > so it has .value property
+    console.log(refContainer.current.value)
+    console.log(divContainer.current) //  return <div>hello world</div>
   }
 
-  console.log(refContainer) // (3c) b4 submission > {current: null}
+  // (3) one thing that we can use with "ref hook" is "focus our input at the moment our app renders" > because it does NOT trigger RERENDER > so that we can use with useEffect() without worrying about dependency list
+  useEffect(() => {
+    console.log(refContainer.current)
+    refContainer.current.focus() // will focus right after page is loaded
+  })
 
   return (
-    <form className='form' onSubmit={handleSubmit}>
-      <div>
-        {/* (2) */}
-        <input type='text' ref={refContainer} />
-        <button type='submit'>submit</button>
-      </div>
-    </form>
+    <React.Fragment>
+      <form className='form' onSubmit={handleSubmit}>
+        <div>
+          <input type='text' ref={refContainer} />
+          <button type='submit'>submit</button>
+        </div>
+      </form>
+
+      {/* (2) */}
+      <div ref={divContainer}>hello world</div>
+    </React.Fragment>
   )
 }
 
