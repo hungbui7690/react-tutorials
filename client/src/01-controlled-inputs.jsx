810b152dd020176ee8c2dec2facc9f7fde9f41b4
Hung Bui <hungbui7690@gmail.com>
2023-03-06 14:42:48 -0700

    Form: User Challenge P1

--- client/src/01-controlled-inputs.jsx
+++ client/src/01-controlled-inputs.jsx
@@ -1,52 +1,60 @@
 /*
-  Form: Controlled Inputs P4
+  Form: User Challenge P1
 
+  - setup controlled input (name input)
+  - setup onSubmit (for now just placeholder)
+  - import data array (first array) from data
+  - create another state value (data as default)
+  - iterate over and display right after form (h4)
+  - when user submits the form add new person to the list
+
+  - Extra Challenge
+    - add button and setup functionality to remove user
+    
 */
 
 import { useState } from 'react'
+import { data } from './data' // (***)
 
-const ControlledInputs = () => {
+const UserChallenge = () => {
+  // (1)
   const [name, setName] = useState('')
-  const [email, setEmail] = useState('') // (***) multiple inputs
-
-  // (***) email must be valid to submit since we use input with type=email > (browser sometimes does not show the form validation)
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(name, email) // (***)
-  }
+  const [users, setUsers] = useState(data)
 
   return (
-    <form className='form' onSubmit={handleSubmit}>
-      <h4>controlled inputs</h4>
-      <div className='form-row'>
-        <label htmlFor='name' className='form-label'>
-          name
-        </label>
-        <input
-          type='text'
-          className='form-input'
-          id='name'
-          value={name} // (1a) link to "name" state
-          onChange={(e) => setName(e.target.value)} // (1b)
-        />
-      </div>
-      <div className='form-row'>
-        <label htmlFor='email' className='form-label'>
-          email
-        </label>
-        <input
-          type='email'
-          className='form-input'
-          id='email'
-          value={email} // (2a) links to "email" state
-          onChange={(e) => setEmail(e.target.value)} // (2b)
-        />
-      </div>
-      <button type='submit' className='btn btn-block'>
-        submit
-      </button>
-    </form>
+    <div>
+      <form className='form'>
+        <h4>Add User</h4>
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            name
+          </label>
+          <input
+            type='text'
+            className='form-input'
+            value={name} // (2a)
+            onChange={(e) => setName(e.target.value)} // (2b)
+            id='name'
+          />
+        </div>
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+
+      {/* render users */}
+      <h2>users</h2>
+      {users.map((user) => {
+        return (
+          <div key={user.id} className='people-section'>
+            <h4>{user.name}</h4>
+            <button className='btn'>remove</button>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
-export default ControlledInputs
+export default UserChallenge
