e57d7c385e04d3a438e946f8f724e324682a272a
Hung Bui <hungbui7690@gmail.com>
2023-03-06 04:08:57 -0700

    useEffect: React Hooks Rules P2

--- client/src/01-useEffect.jsx
+++ client/src/01-useEffect.jsx
@@ -1,17 +1,21 @@
 /*
-  useEffect: React Hooks Rules P1
+  useEffect: React Hooks Rules P2
 */
 
 import { useEffect, useState } from 'react'
 const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturnsFetchData = () => {
-  const [condition, setCondition] = useState(true)
+  const [condition, setCondition] = useState(false)
 
-  // (***) error
   if (condition) {
-    const [state, setState] = useState(false)
+    return <h3>Example</h3>
   }
+
+  // (***) this will create a bunch of warnings and errors as well in create-react-app > don't place useEffect() after "return JSX" > reason: because this will make useEffect() runs conditionally > if condition is true : return > else : run useEffect()
+  useEffect(() => {
+    console.log('Hello')
+  }, [])
 
   return <h2>React Hooks Rules</h2>
 }
