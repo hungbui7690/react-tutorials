e355fb7399388d40c319e8682a33bfe7599d56a6
Hung Bui <hungbui7690@gmail.com>
2023-03-06 04:05:18 -0700

    useEffect: React Hooks Rules P1

--- client/src/01-useEffect.jsx
+++ client/src/01-useEffect.jsx
@@ -1,54 +1,19 @@
 /*
-  useEffect: Fetch Function Location
+  useEffect: React Hooks Rules P1
 */
 
 import { useEffect, useState } from 'react'
 const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturnsFetchData = () => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [isError, setIsError] = useState(false)
-  const [user, setUser] = useState(null)
+  const [condition, setCondition] = useState(true)
 
-  const fetchUser = async () => {
-    try {
-      const resp = await fetch(url)
-      const user = await resp.json()
-      setUser(user)
-    } catch (error) {
-      setIsError(true)
-      console.log(error)
-    }
-    setIsLoading(false)
+  // (***) error
+  if (condition) {
+    const [state, setState] = useState(false)
   }
 
-  useEffect(() => {
-    fetchUser()
-  }, []) // (***) don't add fetchUser to dep list > trigger inf loop
-
-  // (***) ok > but if we move this line b4 useEffect() > error
-  const { avatar_url, name, company, bio } = user
-
-  if (isLoading) {
-    return <h2>Loading...</h2>
-  }
-
-  if (isError) {
-    return <h2>There was an error...</h2>
-  }
-
-  return (
-    <div>
-      <img
-        style={{ width: '150px', borderRadius: '25px' }}
-        src={user.avatar_url}
-        alt={user.name}
-      />
-      <h2>{user.name}</h2>
-      <h4>works at {user.company}</h4>
-      <p>{user.bio}</p>
-    </div>
-  )
+  return <h2>React Hooks Rules</h2>
 }
 
 export default MultipleReturnsFetchData
