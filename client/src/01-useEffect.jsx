817d509a399cbe89cdceac0e32e1dd80fb81ed24
Hung Bui <hungbui7690@gmail.com>
2023-03-06 03:06:59 -0700

    useEffect: Fundamentals P1

--- client/src/01-useEffect.jsx
+++ client/src/01-useEffect.jsx
@@ -1,34 +1,31 @@
 /*
-  useEffect: Why 
+  useEffect: Fundamentals P1
   
-  - initial render - setup state value and invoke sayHello
-  - in the sayHello update state, trigger re-render
+  - useEffect is a hook in React that allows you to perform side effects in function components. There is no need for urban dictionary - basically any work outside of the component. Some examples of side effects are: subscriptions, fetching data, directly updating the DOM, event listeners, timers, etc.
 
-  - re-render - setup state value and invoke sayHello
-  - in the sayHello update state, trigger re-render
-  
-  - repeat
-  - repeat
-  - repeat
-  
-
-  > should we use functional update approach for everything?
-  > useEffect() can run conditionally
+    - useEffect hook
+    - accepts two arguments (second optional)
+    - first argument - cb function
+    - second argument - dependency array
+    - by default runs on each render (initial and re-render)
+    - cb can't return promise (so can't make it async)
+    - if dependency array empty [] runs only on initial render
 */
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-const ComponentExample = () => {
+const UseEffectBasics = () => {
   const [value, setValue] = useState(0)
 
-  // every time this component renders, this function will be re-created
   const sayHello = () => {
     console.log('hello there')
-
-    // setValue(value + 1) // (***) be careful > this will trigger inf loop > since it will trigger re-render (because of setState) > then the sayHello() at (xxx) will be invoke
   }
+  sayHello()
 
-  sayHello() // (xxx) this function be invoke in every render
+  // (***) this will run in every render > default behavior
+  useEffect(() => {
+    console.log('hello from useEffect')
+  })
 
   return (
     <div>
@@ -39,4 +36,4 @@ const ComponentExample = () => {
     </div>
   )
 }
-export default ComponentExample
+export default UseEffectBasics
