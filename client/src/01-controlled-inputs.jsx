666d7aeb81938286e5878675081840114f1b8cd7
Hung Bui <hungbui7690@gmail.com>
2023-03-06 13:16:37 -0700

    Form: Controlled Inputs P2

--- client/src/01-controlled-inputs.jsx
+++ client/src/01-controlled-inputs.jsx
@@ -1,9 +1,24 @@
 /*
-  Form: Controlled Inputs P1: Setup
-  - in this form, we have 1 input
+  Form: Controlled Inputs P2
+  - setup state values
+  - add value and onChange to each input
+  - setup onSubmit
+
+  (***) controlled inputs: inputs are not controlled by themselves, but is being controlled by the state
+
 */
 
+import { useState } from 'react'
+
 const ControlledInputs = () => {
+  const [name, setName] = useState('')
+
+  // (3) browser does not update
+  const handleChange = (e) => {
+    console.log(e.target.name) // (***) the input does not have "name attribute" > empty
+    console.log(e.target.value) // value of the input
+  }
+
   return (
     <form className='form'>
       <h4>controlled inputs</h4>
@@ -11,7 +26,13 @@ const ControlledInputs = () => {
         <label htmlFor='name' className='form-label'>
           name
         </label>
-        <input type='text' className='form-input' id='name' />
+        <input
+          type='text'
+          className='form-input'
+          id='name'
+          value={name} // (2a) hook state to this input
+          onChange={handleChange} // (2b)
+        />
       </div>
       <button type='submit' className='btn btn-block'>
         submit
@@ -19,4 +40,5 @@ const ControlledInputs = () => {
     </form>
   )
 }
+
 export default ControlledInputs
