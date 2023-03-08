bde9a996a63738af60767b9614fcfdb76f14799b
Hung Bui <hungbui7690@gmail.com>
2023-03-08 01:20:36 -0700

    useTransition Hook: Problem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 /*
-  useMemo Hook P4: slowFunction Solution
+  useTransition Hook: Problem
+  - let us update the state without blocking the UI
+
+
+  > for example: when user is typing, it triggers a function and filter 20,000 records > take time > user cannot type (UI is blocking)
+
+  (***) to test > slow down CPU > pic: slow-down-cpu
   
 */
 
 import React from 'react'
-import LowerState from './LowerState'
+import LatestReact from './LatestReact'
 
 const App = () => {
-  return <LowerState />
+  return <LatestReact />
 }
 
 export default App
