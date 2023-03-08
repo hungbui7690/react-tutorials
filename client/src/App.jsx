0033b1d36d84ce0781d6fcc61967574265334598
Hung Bui <hungbui7690@gmail.com>
2023-03-08 03:05:54 -0700

    React Router V6: First Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,37 @@
+/*
+  React Router V6: First Pages
+  > npm install react-router-dom@6
+
+  (***) not from Official React
+
+
+  (1)
+  - setup in App.js
+  
+  <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<div>home page</div>} />
+
+  (2) after setup, we have to routes: / and /testing
+*/
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div className='container'>
-      <h2>App</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>home page</div>}></Route>
+        <Route
+          path='/testing'
+          element={
+            <div>
+              <h2>testing </h2>
+            </div>
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- client/src/data.js
+++ client/src/data.js
@@ -1,23 +1,37 @@
-export const data = [
-  { id: 1, name: 'john' },
-  { id: 2, name: 'peter' },
-  { id: 3, name: 'susan' },
-  { id: 4, name: 'anna' },
-];
-
-export const people = [
-  { id: 1, name: 'bob', nickName: 'Stud Muffin' },
-  { id: 2, name: 'peter' },
+const products = [
   {
-    id: 3,
-    name: 'oliver',
-    images: [
-      {
-        small: {
-          url: 'https://res.cloudinary.com/diqqf3eq2/image/upload/ar_1:1,bo_5px_solid_rgb:ff0000,c_fill,g_auto,r_max,w_1000/v1595959121/person-1_aufeoq.jpg',
-        },
-      },
-    ],
+    id: 'recZkNf2kwmdBcqd0',
+    name: 'accent chair',
+    image:
+      'https://dl.airtable.com/.attachmentThumbnails/e8bc3791196535af65f40e36993b9e1f/438bd160',
   },
-  { id: 4, name: 'david' },
-];
+  {
+    id: 'recEHmzvupvT8ZONH',
+    name: 'albany sectional',
+
+    image:
+      'https://dl.airtable.com/.attachmentThumbnails/0be1af59cf889899b5c9abb1e4db38a4/d631ac52',
+  },
+  {
+    id: 'rec5NBwZ5zCD9nfF0',
+    name: 'albany table',
+
+    image:
+      'https://dl.airtable.com/.attachmentThumbnails/7478483f40a2f56662a87b304bd4e104/707d397f',
+  },
+  {
+    id: 'recd1jIVIEChmiwhe',
+    name: 'armchair',
+
+    image:
+      'https://dl.airtable.com/.attachmentThumbnails/530c07c5ade5acd9934c8dd334458b86/cf91397f',
+  },
+  {
+    id: 'recoM2MyHJGHLVi5l',
+    name: 'bar stool',
+    image:
+      'https://dl.airtable.com/.attachmentThumbnails/a6119fabf7256049cc0e8dbcdf536c9c/b0153f66',
+  },
+]
+
+export default products
