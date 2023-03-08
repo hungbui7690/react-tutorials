e55342fe97a0732f6fcd438aab3c58d613b1b75d
Hung Bui <hungbui7690@gmail.com>
2023-03-08 06:04:20 -0700

    Axios: Setup Headers P2: .get(url, {})

--- client/src/01-axios.jsx
+++ client/src/01-axios.jsx
@@ -1,8 +1,10 @@
 /*
-  Axios: Setup Headers P1
+  Axios: Setup Headers P2: .get(url, {})
 
-  - some server requires Headers
-  - url = 'https://icanhazdadjoke.com/' > this one can returns text/html or application/json or text/plain based on our use > we can pass headers to this server and get what we want > check docs
+  - headers format: 
+      headers: {
+        Accept: 'application/json'
+      }
 
 */
 
@@ -10,14 +12,17 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const url = 'https://icanhazdadjoke.com/'
-// Accept : 'application/json'
 
 const FetchData = () => {
-  const [joke, setJoke] = useState('random dad joke')
+  const [joke, setJoke] = useState('')
 
   const fetchDadJoke = async () => {
     try {
-      const response = await axios(url, {}) // (***) without headers > we don't get the json format as we want > html
+      const response = await axios(url, {
+        headers: {
+          Accept: 'application/json', // (***)
+        },
+      })
       console.log(response)
     } catch (error) {
       console.log(error.response)
