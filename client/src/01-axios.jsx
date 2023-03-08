3d3efa1e460f09171d717e8332e788887e5eea66
Hung Bui <hungbui7690@gmail.com>
2023-03-08 08:22:29 -0700

    Axios: Interceptors

--- client/src/01-axios.jsx
+++ client/src/01-axios.jsx
@@ -1,30 +1,30 @@
 /*
-  Axios: Custom Instance
-  - to fix the issue from the last lesson (global defaults in every request) > we use Custom Instance
-    > with this: when we connect to multiple server APIs > we can setup defaults for each server 
+  Axios: Interceptors
+  - interceptors: 
+    + request: right b4 request is sent to server
+    + response: right b4 response is sent back to client
 
-  (*) remove the global defaults from App.js
-  (1) create axios/custom.js
+
+  (***) in this case, we add interceptors to custom instance 
+    > but interceptors can also be added to any axios request/response > check axios docs
+  
+
+  (1) custom.js
 
 */
 
 import { useEffect } from 'react'
-import axios from 'axios'
 
 // (2)
 import authFetch from './axios/custom'
+import axios from 'axios'
 
-const productsUrl = 'https://course-api.com/react-store-products'
-const randomUserUrl = 'https://randomuser.me/api'
-
-const GlobalInstance = () => {
+const Axios = () => {
   const fetchData = async () => {
     try {
-      // (3) now, we use authFetch() instead of axios.get()
-      const response1 = await authFetch('/react-store-products') // (***) we don't need the full url
+      const response = await authFetch('/react-store-products')
 
-      // (4)
-      const response2 = await axios.get(randomUserUrl)
+      console.log(response)
     } catch (error) {
       console.log(error.response)
     }
@@ -34,6 +34,6 @@ const GlobalInstance = () => {
     fetchData()
   }, [])
 
-  return <h2 className='text-center'>global instance</h2>
+  return <h2 className='text-center'>Interceptors</h2>
 }
-export default GlobalInstance
+export default Axios
--- client/src/axios/custom.js
+++ client/src/axios/custom.js
@@ -1,8 +1,37 @@
 import axios from 'axios'
 
-// (1)
 const authFetch = axios.create({
   baseURL: 'https://course-api.com',
 })
+
+// (***) modify request b4 sending to server
+authFetch.interceptors.request.use(
+  function (request) {
+    console.log('got request')
+
+    return request // (***) must return request
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
+// (***) modify response from server b4 send back to client
+authFetch.interceptors.response.use(
+  function (response) {
+    console.log('got response')
+    response.name = 'Hung' // modify response
+
+    return response // (***) must return response
+  },
+  function (error) {
+    console.log(error.response)
+    if (error.response.status === 404) {
+      // do something
+      console.log('NOT FOUND')
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default authFetch
