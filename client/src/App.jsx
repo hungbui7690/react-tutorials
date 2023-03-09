454fd87e425560ed4a6056b861a7dc3cdd5230d7
Hung Bui <hungbui7690@gmail.com>
2023-03-09 04:03:10 -0700

    Redux: Setup Store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,5 @@
 /*
-  Redux: Initial Setup
-  - pic: cart-exercise
-  - we built this project before with reducer and context API > react-mini-projects: Cart-useReducer
-    > we want to build again using Redux
-
-  (1) check package.json, we need these 2: 
-        "react-redux": "^7.2.0",
-        "redux": "^4.0.5",
-
-/////////////////////////////////////////////
-
-  Redux: Intro
-  - not from official react
-  - very similar to context API > but a little more rigid and opinionated > more structures & more rules
-  - redux can be used with angular/vue as well
-  - use when work with more than 1 person
-  - we don't need to use redux for all projects, but we still need to learn it 
+  Redux: Setup Store 
 
   > pic: redux-intro
   > global states === store > similar to context API
@@ -27,6 +11,17 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import CartContainer from './components/CartContainer'
 import cartItems from './cart-items'
+
+// (1) deprecated > later we will learn Redux Tool Kit
+import { createStore } from 'redux'
+
+// (2)
+function reducer(state, action) {
+  console.log({ state, action }) // {state: undefined, action: {…}}
+}
+
+// (3)
+const store = createStore(reducer)
 
 function App() {
   return (
