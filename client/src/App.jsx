a3966be59bc243f0b05ed3620746aa2e137dbec2
Hung Bui <hungbui7690@gmail.com>
2023-03-09 07:15:11 -0700

    Redux: Actions as Variables P1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 /*
-  Redux: More Complicated State P2
-  - pre ES6: need to use Object.assign() 
-  - ES6: can use spread operator
+  Redux: Actions as Variables
+  - prevent typos
+  - if we have the wrong action > no error now > hard to find where is the error
+
+
 
 */
 
@@ -13,22 +15,27 @@ import { createStore } from 'redux'
 
 const initialStore = {
   count: 78,
-  name: 'Joe Doe', // (***)
+  name: 'Joe Doe',
 }
 
+// (***)
+const DECREASE = 'DECREASE'
+const INCREASE = 'INCREASE'
+const RESET = 'RESET'
+const CHANGE_NAME = 'CHANGE_NAME'
+
 function reducer(state, action) {
-  if (action.type === 'DECREASE') {
-    return { ...state, count: state.count - 1 } // (***) copy
+  // (***) change to variables
+  if (action.type === DECREASE) {
+    return { ...state, count: state.count - 1 }
   }
-  if (action.type === 'INCREASE') {
+  if (action.type === INCREASE) {
     return { ...state, count: state.count + 1 }
   }
-  if (action.type === 'RESET') {
+  if (action.type === RESET) {
     return { ...state, count: 0 }
   }
-
-  // (***)
-  if (action.type === 'CHANGE_NAME') {
+  if (action.type === CHANGE_NAME) {
     return { ...state, name: 'bobo' }
   }
 
@@ -37,16 +44,15 @@ function reducer(state, action) {
 
 const store = createStore(reducer, initialStore)
 
-console.log(store.getState()) // {count: 78, name: 'Joe Doe'}
-store.dispatch({ type: 'DECREASE' })
-console.log(store.getState()) // {count: 77, name: 'Joe Doe'}
-store.dispatch({ type: 'INCREASE' })
-console.log(store.getState()) // {count: 78, name: 'Joe Doe'}
-store.dispatch({ type: 'RESET' })
-console.log(store.getState()) // {count: 0, name: 'Joe Doe'}
-
 // (***)
-store.dispatch({ type: 'CHANGE_NAME' })
+console.log(store.getState()) // {count: 78, name: 'Joe Doe'}
+store.dispatch({ type: DECREASE })
+console.log(store.getState()) // {count: 77, name: 'Joe Doe'}
+store.dispatch({ type: INCREASE })
+console.log(store.getState()) // {count: 78, name: 'Joe Doe'}
+store.dispatch({ type: RESET })
+console.log(store.getState()) // {count: 0, name: 'Joe Doe'}
+store.dispatch({ type: CHANGE_NAME })
 console.log(store.getState()) // {count: 0, name: 'bobo'}
 
 function App() {
