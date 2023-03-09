b94155494f412e3d4891a3ef8737f52447dda599
Hung Bui <hungbui7690@gmail.com>
2023-03-09 12:32:20 -0700

    RTK: Setup Store & Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,7 @@
 /*
-  RTK: Initial Setup
-  > npm install @reduxjs/toolkit react-redux
+  RTK: Setup Store
 
-  - consists of few libraries
-    - redux (core library, state management)
-    - immer (allows to mutate state)
-    - redux-thunk (handles async actions)
-    - reselect (simplifies reducer functions)
+  (1) create store.js
 
 */
 
---
+++ client/src/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+// (1) go to main.jsx
+export const store = configureStore({
+  reducer: {},
+})
